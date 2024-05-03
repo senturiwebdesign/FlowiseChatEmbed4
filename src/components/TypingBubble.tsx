f992b4304a9e4024e4f2c17fb840ec9b4440b759
whispered.ai <whisperedai@gmail.com>
2024-05-03 14:28:02 +0200

    Typing v2

--- src/components/TypingBubble.tsx
+++ src/components/TypingBubble.tsx
@@ -1,7 +1,7 @@
 export const TypingBubble = () => (
-  <div class="flex items-center">
-    <div class="w-2 h-2 mr-1 rounded-full bubble1" />
-    <div class="w-2 h-2 mr-1 rounded-full bubble2" />
-    <div class="w-2 h-2 rounded-full bubble3" />
+  <div class="flex items-center typing_bubble">
+    <div class="w-2 h-2 mr-1 rounded-full bubble1 typing_bubble2" />
+    <div class="w-2 h-2 mr-1 rounded-full bubble2 typing_bubble3" />
+    <div class="w-2 h-2 rounded-full bubble3 typing_bubble4" />
   </div>
 );
