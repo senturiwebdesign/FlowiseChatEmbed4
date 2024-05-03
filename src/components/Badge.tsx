8962b7323fd77b98c78e54817cc9750f72284d88
whispered.ai <whisperedai@gmail.com>
2024-05-03 17:05:27 +0200

    New v12

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -37,7 +37,31 @@ export const Badge = (props: Props) => {
   });
 
   return (
-    <div style="font-size: 12px; display:flex;align-items:center;justify-content:center;background-color:transparent;padding:8px;border-radius:4px;">Powered by 
-<a href="https://whispered.ai/" target="_blank" rel="noopener noreferrer" style="margin-left:2px; display:flex;align-items:center;text-decoration:none;font-weight:bold;color:#333;"><img src="https://whispered.ai/wp-content/uploads/2023/06/favicon.png" alt="logo" style="width:15px;height:15px;vertical-align:middle; margin-right:2px;"/><span style="letter-spacing: -1px;">whispered.ai</span></a></div>
+    <div
+      style={{
+        'font-size': '12px',
+        display: 'flex',
+        'align-items': 'center',
+        'justify-content': 'center',
+        'background-color': 'transparent',
+        padding: '8px',
+        'border-radius': '4px',
+      }}
+    >
+      Powered by
+      <a
+        href="https://whispered.ai/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ 'margin-left': '2px', display: 'flex', 'align-items': 'center', 'text-decoration': 'none', 'font-weight': 'bold', color: '#333' }}
+      >
+        <img
+          src="https://whispered.ai/wp-content/uploads/2023/06/favicon.png"
+          alt="logo"
+          style={{ width: '15px', height: '15px', 'vertical-align': 'middle', 'margin-right': '2px' }}
+        />
+        <span style={{ 'letter-spacing': '-1px' }}>whispered.ai</span>
+      </a>
+    </div>
   );
 };
